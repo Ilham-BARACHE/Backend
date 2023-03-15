21372ad0bbd7cf04a43e2c2f1ce67f4d200e4ec4
Ilham-barache <ilhemmbarachee@gmail.com>
2023-03-15 13:32:05 +0100

    apiheurecommune

--- src/main/java/org/example/controller/SamTrainController.java
+++ src/main/java/org/example/controller/SamTrainController.java
@@ -45,14 +45,12 @@ public class SamTrainController {
 
     private final TrainRepository trainRepository;
 
-    private  final MrRepository mrRepository;
+    private final MrRepository mrRepository;
 
     private final M_50592Repository m50592Repository;
 
 
-
-
-    public SamTrainController(SamRepository samRepository, TrainRepository trainRepository,MrRepository mrRepository,M_50592Repository m50592Repository) {
+    public SamTrainController(SamRepository samRepository, TrainRepository trainRepository, MrRepository mrRepository, M_50592Repository m50592Repository) {
         this.samRepository = samRepository;
         this.trainRepository = trainRepository;
         this.mrRepository = mrRepository;
@@ -119,5 +117,84 @@ public class SamTrainController {
     }
 
 
+    @GetMapping("/info")
+    public ResponseEntity<List<Map<String, Object>>> getBySiteAndDateFichierheure(
+            @RequestParam("site") String site,
+            @RequestParam("dateFichier") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) java.time.LocalDate date) {
+
+        Date dateFichier = Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        List<Sam> sams = samRepository.findBySiteAndDateFichier(site, dateFichier);
+        List<Train> trains = trainRepository.findBySiteAndDateFichier(site, dateFichier);
+        List<M_50592> m50592s = m50592Repository.findBySiteAndDateFichier(site, dateFichier);
+
+        // Créer un Set pour stocker toutes les heures des enregistrements
+        Set<Date> heuresCommunes = new HashSet<>();
+        for (Sam sam : sams) {
+            heuresCommunes.add(sam.getHeureFichier());
+        }
+        for (Train train : trains) {
+            heuresCommunes.add(train.getHeureFichier());
+        }
+        for (M_50592 m50592 : m50592s) {
+            heuresCommunes.add(m50592.getHeureFichier());
+        }
+
+        List<Map<String, Object>> result = new ArrayList<>();
+
+        // Itérer sur les heures communes et récupérer les enregistrements correspondants
+        for (Date heureCommune : heuresCommunes) {
+            List<Sam> samsHeureCommune = sams.stream()
+                    .filter(sam -> sam.getHeureFichier().equals(heureCommune))
+                    .collect(Collectors.toList());
+            List<Train> trainsHeureCommune = trains.stream()
+                    .filter(train -> train.getHeureFichier().equals(heureCommune))
+                    .collect(Collectors.toList());
+            List<M_50592> m50592sHeureCommune = m50592s.stream()
+                    .filter(m50592 -> m50592.getHeureFichier().equals(heureCommune))
+                    .collect(Collectors.toList());
+
+            // Ajouter les enregistrements correspondants dans la liste result
+            for (Sam sam : samsHeureCommune) {
+                Map<String, Object> samMap = new HashMap<>();
+                samMap.put("id", sam.getId());
+                samMap.put("NbEssieux", sam.getNbEssieux());
+                samMap.put("url", sam.getUrl());
+                samMap.put("Statut", sam.getStatut());
+                samMap.put("NbOccultations", sam.getNbOccultations());
+                samMap.put("vitesse_moy", sam.getVitesse_moy());
+                result.add(samMap);
+            }
+
+            for (Train train : trainsHeureCommune) {
+                Map<String, Object> trainMap = new HashMap<>();
+                trainMap.put("id", train.getId());
+                trainMap.put("numTrain", train.getNumTrain());
+
+                Mr mr = mrRepository.findByNumTrain(train.getNumTrain());
+                if (mr != null) {
+                    trainMap.put("mr", mr.getMr());
+                }
+
+                result.add(trainMap);
+            }
+
+            for (M_50592 m50592 : m50592sHeureCommune) {
+                Map<String, Object> m50592Map = new HashMap<>();
+                m50592Map.put("id", m50592.getId());
+                m50592Map.put("villeArrivee", m50592.getEnvironnement().getVilleArrivee());
+                m50592Map.put("villeDepart", m50592.getEnvironnement().getVilleDepart());
+                m50592Map.put("meteo", m50592.getEnvironnement().getMeteo());
+
+
+                result.add(m50592Map);
+            }
+
+        }
+
+        return ResponseEntity.ok(result);
+
+
+    }
 
 }
