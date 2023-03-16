61b4852194dc25f10beafdb7fd2587f724028158
Ilham-barache <ilhemmbarachee@gmail.com>
2023-03-16 11:23:00 +0100

    crossorigin

--- src/main/java/org/example/controller/SamTrainController.java
+++ src/main/java/org/example/controller/SamTrainController.java
@@ -17,10 +17,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.TemporalType;
 import java.sql.Time;
@@ -32,7 +29,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/")
+@CrossOrigin("http://localhost:3000/")
 public class SamTrainController {
 
 
